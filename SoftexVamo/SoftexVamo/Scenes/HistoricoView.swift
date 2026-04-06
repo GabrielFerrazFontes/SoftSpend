cbc37af272daf8d39afcdf1b40b4e8a159d3b546
joaofarhat <joaofarhat9@gmail.com>
2026-04-06 18:01:50 -0300

    Histórico de Ciclos

---
+++ SoftexVamo/SoftexVamo/Scenes/HistoricoView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoricoView.swift
+//  SoftexVamo
+//
+//  Created by Joao Victor on 06/04/26.
+//
+
+import SwiftUI
+
+struct HistoricoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoricoView()
+}
