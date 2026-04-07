6d8bcc2f127a12f1512dd7411aa2658457549f94
Gabriel Ferraz Fontes <brielff@gmail.com>
2026-04-07 15:08:01 -0300

    Add widgetkit

---
+++ SoftexVamo/SoftexWidget/SoftexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SoftexWidgetBundle.swift
+//  SoftexWidget
+//
+//  Created by Gabriel fontes on 07/04/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SoftexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SoftexWidget()
+    }
+}
